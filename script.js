a5c916f0e213fd3600de8bb45a30efe3bf16c649
Cesar Silva <cas.silva@live.com>
2020-11-16 07:33:43 -0300

    Conclusão do exercício - projeto

--- script.js
+++ script.js
@@ -12,15 +12,39 @@ function verificar() {
 
     } else {
         var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(sex[0].checked) {
             genero ='masculino'
+
+            if(idade >=0 && idade < 10){
+                img.setAttribute('src', './images/menino.jpg')
+            } else if(idade < 21) {
+                img.setAttribute('src', './images/moço.jpg')
+            } else if(idade < 50) {
+                img.setAttribute('src', './images/adulto.jpg')
+            }else {
+                img.setAttribute('src', './images/idoso.jpg')
+            }
         } else if(sex[1].checked) {
             genero ='feminino'
-        }
-        var idade = ano - Number(fano.value) 
-        //window.alert(idade)
-       res.innerHTML = `A idade calculada é ${idade} e o sexo ${genero}`
 
+            if(idade >=0 && idade < 10){
+                img.setAttribute('src', './images/menina.jpg')
+            } else if(idade < 21) {
+                img.setAttribute('src', './images/moça.jpg')
+            } else if(idade < 50) {
+                img.setAttribute('src', './images/adulta.jpg')
+            }else {
+                img.setAttribute('src', './images/idosa.jpg')
+            }
+        }
+        
+        res.style.textAlign = 'center'
+        //window.alert  (idade)
+        res.innerHTML = `A idade calculada é ${idade} e o sexo ${genero}`
+        res.appendChild(img)
     }
 }
